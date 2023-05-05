94c4db19aea747882fdbde7d481164eeda55cf54
GundaAruna <130962449+GundaAruna@users.noreply.github.com>
2023-05-05 21:27:19 +0530

    Create AddofTwo.java

---
+++ AddofTwo.java
@@ -0,0 +1,19 @@
+package mypackage;
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a;
+		int b;
+		System.out.print("Enter the value of a: ");
+		a=sc.nextInt();
+		System.out.print("Enter the value of b: ");
+		b=sc.nextInt();
+		System.out.print("The sum is:"+(a+b));
+
+	}
+
+}
